8dc402dabcadec7a3cb1d14a91094c36ed8754d5
PriteryTivi <channelnnt1@gmail.com>
2026-01-14 11:41:07 +0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: 0");
+header("X-Frame-Options: DENY");
+header("Content-Security-Policy: frame-ancestors 'none';");
+header("Permissions-Policy: camera=(), microphone=(), geolocation=()");
+http_response_code(200);
+?>
+
 <!DOCTYPE html>
 
 <html lang="vi">
